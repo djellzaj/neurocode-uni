5bff4d6c71ba82d8ab0a841440672c3e48e97100
LuljetaHoti <luljeta.hoti@student.uni-pr.edu>
2026-04-26 23:36:11 +0200

    Krijova index.php dhe e lidha me header.php, nav.php dhe footer.php për strukturë të unifikuar dhe përdorim të include/require.

--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,6 @@
-<?php echo "Home Page"; ?>
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/nav.php'; ?>
+
+<h1>Mirë se vini në NeuroCode</h1>
+
+<?php include '../includes/footer.php'; ?>
