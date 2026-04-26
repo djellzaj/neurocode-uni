8b59a07456df09de00636a3f588b4b27cea4cb9e
djellzaj <djellza.jakupi@student.uni-pr.edu>
2026-04-26 15:15:25 +0200

    Shtova faqen per regjistrim

---
+++ pages/register.php
@@ -0,0 +1,32 @@
+<?php
+// register.php
+?>
+
+<!DOCTYPE html>
+<html lang="sq">
+<head>
+    <meta charset="UTF-8">
+    <title>Regjistrimi - NeuroCode</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+
+<h1>Regjistrohu në NeuroCode</h1>
+
+<form method="POST" action="">
+    <label>Emri:</label>
+    <input type="text" name="name" required>
+
+    <label>Email:</label>
+    <input type="email" name="email" required>
+
+    <label>Fjalëkalimi:</label>
+    <input type="password" name="password" required>
+
+    <button type="submit">Regjistrohu</button>
+</form>
+
+<p>Ke llogari? <a href="login.php">Kyçu këtu</a></p>
+
+</body>
+</html>
