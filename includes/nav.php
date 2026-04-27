dab40ed772c7be05d0ad7e2b6935e18fa510294f
djellzaj <djellza.jakupi@student.uni-pr.edu>
2026-04-27 17:53:39 +0200

    Rregullova linkat e navigimit

--- includes/nav.php
+++ includes/nav.php
@@ -6,10 +6,9 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <nav>
     <a href="../pages/index.php">Ballina</a>
-    <a href="../pages/about.php">Rreth Nesh</a>
-    <a href="../pages/services.php">Shërbimet</a>
+    <a href="../pages/login.php">Kyçu</a>
+    <a href="../pages/register.php">Regjistrohu</a>
     <a href="../pages/contact.php">Kontakt</a>
-    <a href="../pages/help.php">Ndihmë</a>
 
      <a href="../pages/login.php" class="login-btn">Login</a>
 </nav>
