4eba52df8dad504ea58d052b7d4d9b90a998e515
DeaBeqiri <dea.beqiri@student.uni-pr.edu>
2026-04-27 15:30:01 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,19 @@
 <?php
-session_start();
+$theme = "light";
+
+if (isset($_COOKIE["theme"])) {
+    $theme = $_COOKIE["theme"];
+}
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="sq">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NeuroCode</title>
-    <link rel="stylesheet" href="../assets/style.css">
+
+  
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
