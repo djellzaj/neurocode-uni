facfe313da6e787d6a036f2590a662a98e385e24
LuljetaHoti <luljeta.hoti@student.uni-pr.edu>
2026-05-26 00:32:57 +0200

    Implementim i fshirjes së klientëve nga databaza me kontroll të session-it

--- pages/client-delete.php
+++ pages/client-delete.php
@@ -1,1 +1,20 @@
+<?php
+require_once "../config/db.php";
+session_start();
 
+if (!isset($_SESSION["user_email"])) {
+    header("Location: login.php");
+    exit;
+}
+
+$id = $_GET["id"] ?? null;
+
+if (!$id) {
+    die("ID nuk është valide");
+}
+
+$stmt = $conn->prepare("DELETE FROM klientet WHERE id = ?");
+$stmt->execute([$id]);
+
+header("Location: clients.php");
+exit;
